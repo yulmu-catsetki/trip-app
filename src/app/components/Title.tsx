6b588f8af901b1913bda122e6a4216556e1708f1
YULMU <83626845+yulmu-catsetki@users.noreply.github.com>
2024-06-02 16:33:54 +0900

    Update Title.tsx

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 
 interface TitleProps {
   text: string;
+
 }
 
 const Title: React.FC<TitleProps> = ({ text }) => {
   return (
-    <div className="text-center text-2xl font-bold leading-10 my-12 ">
+    <div className="relative inline-block m-10">
+      <span className="text-2xl md:text-3xl font-bold">
         {text}
+      </span>
+      <span className="absolute -bottom-1 left-0 w-full h-1 bg-gradient-to-r from-green-300 via-teal-500 to-emerald-600 rounded-full"></span>
     </div>
-
   );
 };
 
